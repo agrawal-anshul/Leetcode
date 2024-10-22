a2d861ea403ffdced3c812db22aa23bcffa99208
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2024-10-22 19:39:14 +0530

    Time: 0 ms (100%), Space: 10.4 MB (80.74%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int carry = 0;
-        int res = 0;
-        for(int i=digits.size()-1;i>=0;i--)
-        {
-            if(i==digits.size()-1){
-                res = digits[i]+1;
-                carry = res/10;
-                digits[i] = res%10;
-            }else{
-                if(carry==0){
-                    return digits;
-                }
-                else{
-                    res=digits[i]+carry;
-                    carry = res/10;
-                    digits[i] = res%10;
-                }
+    //     int carry = 0;
+    //     int res = 0;
+    //     for(int i=digits.size()-1;i>=0;i--)
+    //     {
+    //         if(i==digits.size()-1){
+    //             res = digits[i]+1;
+    //             carry = res/10;
+    //             digits[i] = res%10;
+    //         }else{
+    //             if(carry==0){
+    //                 return digits;
+    //             }
+    //             else{
+    //                 res=digits[i]+carry;
+    //                 carry = res/10;
+    //                 digits[i] = res%10;
+    //             }
+    //         }
+    //     }
+    //     if(carry!=0){
+    //         digits.insert(digits.begin(),carry);
+    //     }
+    // return digits;
+    // }
+
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] + 1 != 10) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+            if (i == 0) {
+                digits.insert(digits.begin(), 1);
+                return digits;
             }
         }
-        if(carry!=0){
-            digits.insert(digits.begin(),carry);
-        }
-    return digits;
+        return digits;   
+
     }
 };
