a4334412e85899561eb609f48de6338573a6600c
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-01-01 16:56:53 +0530

    Time: 0 ms (100%), Space: 10.3 MB (11.84%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
+    static string toPalindromeString(string s){
+        string t;
+        for(const auto& a:s)
+        {
+            if(a>='A' && a<='Z')
+            {
+                t.push_back(a-'A' + 'a');
+            }
+            else if(a>='a' && a<='z')
+            {
+                t.push_back(a);
+            }
+            else if(a>='0' && a<='9')
+            {
+                t.push_back(a);
+            }
+        }
+        return t;
+    }
     bool isPalindrome(string s) {
-        int l=0,r=s.size()-1;
-        while(l<=r){
-            while(l<r && !isalnum(s[l])){
-                l++;
-            }
-            while(l<r && !isalnum(s[r])){
-                r--;
-            }
-            if(tolower(s[l])!= tolower(s[r])){
+        string s1=toPalindromeString(s);
+        int len = s1.size();
+        for(int i=0; i<len/2; ++i)
+        {
+            if(s1[i] != s1[len-i-1])
+            {
                 return false;
             }
-            l++;
-            r--;
         }
-    return true;
+        return true;
     }
 };
