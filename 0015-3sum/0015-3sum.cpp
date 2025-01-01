0eae7a654b46af0f92f71aa93fd448c491a49895
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-01-01 18:19:06 +0530

    Time: 46 ms (77.11%), Space: 29.2 MB (47.43%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-    // *No need to use sets as to skip duplicates. Instead we are sorting the vector and skipping over duplicate elements* 
-        sort(nums.begin(),nums.end());
+    vector<vector<int>> threeSum(vector<int>& nums,int target = 0) {
         vector<vector<int>>res;
+        // As we are not concerned with the indexes and have to return the elements we can sort the original array.
+        sort(nums.begin(),nums.end());
+        
         for(int i=0;i<nums.size();i++){
-            // Skip duplicates for the first number
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int j=i+1;
-            int k=nums.size()-1;
-            while(j<k)
-            {
-                int sum =nums[i]+nums[j]+nums[k];
-                if(sum == 0)
-                {
-                    res.push_back({nums[i],nums[j],nums[k]});
-
-                    // Skip duplicates for the 2nd number
-                    while(j<k && nums[j]==nums[j+1])j++;
-                    // Skip duplicates for the 3rd number
-                    while(j<k && nums[k]==nums[k-1])k--;
-
-                    j++;
-                    k--;
+            // skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            } 
+            int reqSum = target - nums[i];
+            int l=i+1,r=nums.size()-1;
+            while(l<r){
+                int sum = nums[l] + nums[r];
+                if(sum == reqSum){
+                    res.push_back({nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+                    
+                    // skip duplicates
+                    while (l < r && nums[l-1] == nums[l]) l++;
+                    while (l < r && nums[r+1] == nums[r]) r--;
                 }
-                else if(sum<0)
-                {
-                    j++;
+                else if(sum<reqSum){
+                        l++;
                 }
                 else{
-                    k--;
-                } 
+                        r--;
+                }
             }
-            
         }
         return res;
     }
