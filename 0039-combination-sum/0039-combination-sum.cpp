389325cfd866bf53569aeb44e61c015166ac698d
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-03-15 18:54:12 +0530

    Time: 0 ms (100%), Space: 14.2 MB (54.63%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,31 +1,26 @@
 class Solution {
     void combinationSum(vector<int>& candidates, int idx, int target, vector<int>& selection,vector<vector<int>>& res){
-        if(idx<candidates.size()){
-            if (target < 0) {
-                return;
-            }
-            if(target == 0){
-                res.push_back(selection);
-                return;
-            }
-        
-            // take
-            selection.push_back(candidates[idx]);
-            int newSum=target-candidates[idx];
-            combinationSum(candidates,idx,newSum,selection,res);
-
-            // not-take
-            selection.pop_back();
-            combinationSum(candidates,idx+1,target,selection,res);
+        if (target < 0) {
+            return;
         }
-        
+        if(target == 0){
+            res.push_back(selection);
+            return;
+        }
+    
+        for(int i=idx;i<candidates.size();i++){
+            if (candidates[i] > target)break;
+            selection.push_back(candidates[i]);
+            combinationSum(candidates,i,target-candidates[i],selection,res);
+            selection.pop_back();
+        }
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
         vector<vector<int>>res;
         vector<int>selection;
         combinationSum(candidates,0,target,selection,res);
-
         return res;
     }
 };
