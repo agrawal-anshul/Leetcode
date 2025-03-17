0c01bfe571c6da247ffe89f9186f116018310875
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-03-17 22:39:28 +0530

    Time: 1 ms (80.04%), Space: 10.2 MB (55.78%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    bool isSafe(int row, int col, vector<string>& board, int n){
+        // Check Vertically
+        for(int i = row-1; i >= 0 ; i--){
+            if(board[i][col] == 'Q') return false;
+        }
+        
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        
+        return true;
+    }
+
+    void NQueens(int n, int row, vector<string>& currRes, vector<vector<string>>&res){
+        if(row == n){
+            res.push_back(currRes);
+            return;
+        }
+        for(int col=0; col < n; col++){
+            if(isSafe(row, col, currRes, n)){
+                currRes[row][col]='Q';
+                NQueens(n, row+1, currRes, res);
+                currRes[row][col]='.'; //Backtrack
+            }
+        }
+    } 
+
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>res;
+        vector<string>currRes(n,string(n,'.'));
+        NQueens(n, 0, currRes, res);       
+        return res;
+    }
+};
