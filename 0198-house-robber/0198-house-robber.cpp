d378dc96d6081ebc3e62a376de6550cfccacbbdd
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-05-07 22:33:39 +0530

    Time: 0 ms (100%), Space: 10.7 MB (45.41%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,27 +1,48 @@
 class Solution {
     int rob_recursive(vector<int>& nums, int ind) {
-        if(ind == 0) return nums[0];
         if(ind < 0)return 0;
+        if(ind == 0) return nums[0];
         int pick = nums[ind] + rob_recursive(nums, ind -2);
         int notpick = 0 + rob_recursive(nums, ind - 1);
         return max(pick, notpick);
     }
 
     int rob_memoization(vector<int>& nums, int ind, vector<int>& dp) {
-        if(ind == 0) return nums[0];
         if(ind < 0) return 0;
+        if(ind == 0) return nums[0];
         if(dp[ind] != -1) return dp[ind];
+
         int pick = nums[ind] + rob_memoization(nums, ind - 2, dp);
         int notpick = 0 + rob_memoization(nums, ind - 1, dp);
+        
         return dp[ind] = max(pick, notpick);
+    }
+
+    int rob_tabulation(vector<int>&nums){
+        int n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        vector<int>dp(n,0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[1], nums[0]);
+
+        for(int i = 2; i<n ; i++){
+            int pick = nums[i] + dp[i-2];
+            int notpick = 0 + dp[i-1];
+            dp[i] = max(pick , notpick);
+        }
+        return dp[n-1];
     }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp (nums.size(), -1);
         // return rob_recursive(nums,n-1);
-        return rob_memoization(nums,n-1,dp);
 
+        // vector<int> dp (nums.size(), -1);
+        // return rob_memoization(nums,n-1,dp);
+
+        return rob_tabulation(nums);
 
     }
 };
