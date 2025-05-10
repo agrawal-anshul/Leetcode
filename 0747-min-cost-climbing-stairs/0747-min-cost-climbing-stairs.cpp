40251717f8428d3313e4eb27cf080d346401e723
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-05-10 21:41:08 +0530

    Time: 0 ms (100%), Space: 17.6 MB (72.38%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -38,6 +38,17 @@ public:
         }
         return min(dp[n - 1], dp[n - 2]);
     }
+    int spaceOptimizedFn(int n, vector<int>& dp, vector<int>& cost){
+        int prev2 = cost[0];
+        int prev1 = cost[1];
+        int curr = 0;
+        for(int i = 2 ; i < n ; i++){
+            curr = cost[i] + min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return min(prev1, prev2);
+    }
 
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
