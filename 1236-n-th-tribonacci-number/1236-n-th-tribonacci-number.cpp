4e31facb5bbf1058985c1381bf384fb422abfd09
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-05-10 20:19:55 +0530

    Time: 0 ms (100%), Space: 8.5 MB (22.1%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -0,0 +1,58 @@
+class Solution {
+        int tribonacciRecursive(int n){
+            if(n<2){
+                return 1;
+            }
+            if(n==2){
+                return 2;
+            }
+            return tribonacciRecursive(n-1)+tribonacciRecursive(n-2)+tribonacciRecursive(n-3);
+        };
+
+        int tribonacciMemo(int n, vector<int>& dp){
+            if(n<2){
+               return dp[n] = 1;
+            }
+
+            if(dp[n]!= -1)return dp[n];
+
+            if(n==2){
+                return dp[n] = 2;
+            }
+            return dp[n] = tribonacciMemo(n-1, dp)+tribonacciMemo(n-2, dp)+tribonacciMemo(n-3, dp);
+        };
+
+        int tribonacciBottomUp(int n, vector<int>& dp){
+            if(n<2){
+               return dp[n] = 1;
+            }
+
+            if(n==2){
+                return dp[n] = 2;
+            }
+
+            if(dp[n]!= -1)return dp[n];
+            dp[0] = dp[1] = 1;
+            dp[2] = 2;
+
+            for(int i = 3; i<=n; i++){
+                dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            }
+            return dp[n];
+        };
+public:
+    int tribonacci(int n) {
+        //   Terms:  1 1 2 4 7 ...
+        //   Index:  0 1 2 3 4 ...
+        //nth term:  1 2 3 4 5 ...
+
+        if(n==0) return 0;
+        // return tribonacciRecursive(n-1);
+
+        vector<int>dp(n+1,-1);
+        // return tribonacciMemo(n-1, dp);
+        return tribonacciBottomUp(n-1, dp);
+        
+
+    }
+};
