c32e8aff42c88dcb83eb3bb46580d994663721e7
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-05-31 22:13:59 +0530

    Time: 2 ms (82.29%), Space: 11.2 MB (96.57%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -19,10 +19,45 @@ public:
         }
     }
 
+    // M-2 mimicking recursion call stack using a stack 
+    void generateParenthesisIter(int n, vector<string>& res) {
+        stack<tuple<string, int, int>> stk;
+        // Initial state: empty string, 0 open, 0 close parentheses
+        stk.push({"", 0, 0});
+
+        // Iterate while there are states to explore
+        while (!stk.empty()) {
+            // Retrieve and remove top state from the stack
+            tuple<string, int, int> state = stk.top();
+            stk.pop();
+
+            string current = get<0>(state);
+            int open = get<1>(state);
+            int close = get<2>(state);
+
+            // If we have used all 2n parentheses, add to result
+            if (current.length() == 2 * n) {
+                res.push_back(current);
+                continue;
+            }
+
+            // If we can still add an open '(', push new state with one more open
+            if (open < n) {
+                stk.push({current + '(', open + 1, close});
+            }
+
+            // If we can add a close ')' without violating rules, push new state
+            if (close < open) {
+                stk.push({current + ')', open, close + 1});
+            }
+        }
+    }
+
     vector<string> generateParenthesis(int n) {
         vector<string> res;
         string curr;
-        backtrack(res, curr, 0, 0, n);
+        // backtrack(res, curr, 0, 0, n);
+        generateParenthesisIter(n , res);
         return res;
     }
 };
