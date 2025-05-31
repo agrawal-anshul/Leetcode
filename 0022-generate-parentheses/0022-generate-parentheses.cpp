6fc17918875f7ba4759651171dc5fa203412ad11
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-05-31 21:41:43 +0530

    Time: 3 ms (71.61%), Space: 13 MB (83.72%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    void backtrack(vector<string>& res, string curr, int open, int close, int n) {
-        if (close == n) {
+    void backtrack(vector<string>& res, string& curr, int open, int close, int n) {
+        if (curr.length() == 2 * n) {
             res.push_back(curr);
             return;
         }
 
         if (open < n) {
-            backtrack(res, curr + '(', open + 1, close, n);
+            curr.push_back('(');
+            backtrack(res, curr, open + 1, close, n);
+            curr.pop_back();  // backtrack
         }
 
-        if (open > close) {
-            backtrack(res, curr + ')', open, close + 1, n);
+        if (close < open) {
+            curr.push_back(')');
+            backtrack(res, curr, open, close + 1, n);
+            curr.pop_back();  // backtrack
         }
     }
 
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        backtrack(res, "", 0, 0, n);
+        string curr;
+        backtrack(res, curr, 0, 0, n);
         return res;
     }
 };
