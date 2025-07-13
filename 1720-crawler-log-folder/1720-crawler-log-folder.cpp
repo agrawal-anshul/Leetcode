c2db7ec8196a94910244706279abc3ead2779ffd
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-07-13 18:21:10 +0530

    Time: 0 ms (100%), Space: 13.9 MB (84.5%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int res = 0;
+        for (int i = 0; i < logs.size(); i++) {
+            if (logs[i] == "../") {
+                if (res > 0) res--;
+            } else if (logs[i] == "./") {
+                // Do nothing
+            } else {
+                res++;
+            }
+        }
+        return res;
+    }
+};
