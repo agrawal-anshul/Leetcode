9920fe37ba75dd90a0f80d5687ad03fb0de39411
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-07-13 22:22:38 +0530

    Time: 2057 ms (5%), Space: 472.3 MB (5.01%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,58 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums,int target = 0) {
-        vector<vector<int>>res;
-        // As we are not concerned with the indexes and have to return the elements we can sort the original array.
-        sort(nums.begin(),nums.end());
-        
-        for(int i=0;i<nums.size();i++){
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // Sort the array to ensure consistent triplet ordering
+        sort(nums.begin(), nums.end());
+        int n = (int)nums.size();
+        vector<vector<int>> uniqueTriplets;
+
+        // Outer loop fixes one element and reduces problem to 2Sum
+        for (int i = 0; i < n - 2; i++) {
             // skip duplicates for nums[i]
             if (i > 0 && nums[i] == nums[i - 1]){
                 continue;
             } 
-            int reqSum = target - nums[i];
             
-            // HASHING SOLUTION:
-            /*
-            unordered_map<int,int>mp;
-            for(int j=i+1;j<nums.size();j++){
-                int complement = reqSum-nums[j];
-                if(mp.find(complement) != mp.end()){
-                    res.push_back({nums[i],nums[j],complement});
-                    
+            int target = -nums[i];
+            unordered_map<int, int> mp;
+
+            for (int j = i + 1; j < n; j++) {
+                int complement = target - nums[j];
+
+                // Check if complement already seen for this i
+                if (mp.find(complement) != mp.end()) {
+                    vector<int> triplet = {nums[i], nums[j], complement};
+                    // sort(triplet.begin(), triplet.end());
+                    uniqueTriplets.push_back(triplet); 
+
                     // Skip duplicates for nums[j]
                     while (j + 1 < nums.size() && nums[j] == nums[j + 1]) j++;
                 }
-                mp[nums[j]] = j;
+
+                mp[nums[j]] = j; // store current num with its index
             }
-            */
-
-            // TWO POINTER SOLUTION:
-            
-            int l=i+1,r=nums.size()-1;
-            while(l<r){
-                int sum = nums[l] + nums[r];
-                if(sum == reqSum){
-                    res.push_back({nums[i], nums[l], nums[r]});
-                    l++;
-                    r--;
-                    
-                    // skip duplicates
-                    while (l < r && nums[l-1] == nums[l]) l++;
-                    while (l < r && nums[r+1] == nums[r]) r--;
-                }
-                else if(sum<reqSum){
-                        l++;
-                }
-                else{
-                        r--;
-                }
-            } 
-            
-
-            
-
         }
-        return res;
+        return uniqueTriplets;
     }
 };
