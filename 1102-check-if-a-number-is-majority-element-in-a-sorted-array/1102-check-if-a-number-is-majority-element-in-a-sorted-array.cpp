5ad9f108c3b1435949ae1dc8c8c20f747622f0f1
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-08-09 19:40:54 +0530

    Time: 0 ms (100%), Space: 11.1 MB (42.42%) - LeetHub

--- 1102-check-if-a-number-is-majority-element-in-a-sorted-array/1102-check-if-a-number-is-majority-element-in-a-sorted-array.cpp
+++ 1102-check-if-a-number-is-majority-element-in-a-sorted-array/1102-check-if-a-number-is-majority-element-in-a-sorted-array.cpp
@@ -1,11 +1,46 @@
+// HASHING
+// class Solution {
+// public:
+//     bool isMajorityElement(vector<int>& nums, int target) {
+//         unordered_map<int,int>mp;
+//         for(int i =0;i<nums.size();i++){
+//             mp[nums[i]]++;
+//         }
+//         if(mp[target] > nums.size()/2){return true;}
+//         return false;
+//     }
+// };
+
+
+// Binary Search (As the array is sorted, check if:  start index + n/2 == target)
 class Solution {
+    int binarySearch(vector<int>& nums,int l, int r, int target){
+        if (l > r) return -1;
+
+        int m = l + (r-l)/2 ;
+        int mid = nums[m];
+        int ans = -1;
+        if(target == mid){
+            // look if theres an element before it
+            ans = binarySearch(nums, l, m-1, target);
+            return ans == -1 ? m : ans;
+        }else if(mid > target){
+            // look left
+            ans = binarySearch(nums, l, m-1, target);
+        }else{
+            // look right
+            ans = binarySearch(nums, m+1, r, target);
+        }
+        return ans;
+    }
 public:
     bool isMajorityElement(vector<int>& nums, int target) {
-        unordered_map<int,int>mp;
-        for(int i =0;i<nums.size();i++){
-            mp[nums[i]]++;
+        int n = nums.size();
+        int start_idx = binarySearch(nums,0,n-1, target);
+        if(start_idx == -1)return false;
+        if( (start_idx + n/2) < n  && nums[start_idx + n/2] == target){
+            return true;
         }
-        if(mp[target] > nums.size()/2){return true;}
-        return false;
+    return false;
     }
 };
