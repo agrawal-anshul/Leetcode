d1c6ece9c81296df9ea437a49021ae6d87172088
ANSHUL KUMAR <51377801+agrawal-anshul@users.noreply.github.com>
2025-08-09 19:18:07 +0530

    Time: 3 ms (1.01%), Space: 11.6 MB (10.1%) - LeetHub

---
+++ 1102-check-if-a-number-is-majority-element-in-a-sorted-array/1102-check-if-a-number-is-majority-element-in-a-sorted-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isMajorityElement(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
+        for(int i =0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        if(mp[target] > nums.size()/2){return true;}
+        return false;
+    }
+};
