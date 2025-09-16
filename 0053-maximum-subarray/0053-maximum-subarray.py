062869183883126a7a495af2b09d25acf7d2eae6
Anshul <51377801+agrawal-anshul@users.noreply.github.com>
2025-09-17 02:36:57 +0530

    Time: 51 ms (82.81%), Space: 21.9 MB (23.77%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,26 +1,21 @@
-class Solution:
-    # def maxSubArray(self, nums: List[int]) -> int:
-    #     n = len(nums)
-    #     maxSum = float(-inf)
-    #     i = 0
-    #     j = 0
-    #     currSum = 0
-    #     while i<=j and i < n and j < n:
-    #         currSum += nums[j]
-    #         if currSum > maxSum:
-    #             maxSum = currSum
-    #         if currSum < 0:
-    #             i = j + 1
-    #             j = i
-    #             currSum = 0
-    #         else:
-    #             j = j + 1
-    #     return maxSum
-
-    # M-2
-    def maxSubArray(self, nums: List[int]) -> int:
-        best = cur = nums[0]
-        for x in nums[1:]:
-            cur = max(x, cur + x)   # extend or restart
-            best = max(best, cur)
-        return best
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        l = 0
+        r = 0
+        currSum = 0
+        maxSum = nums[0]
+        while l<=r and r<n:
+            currSum += nums[r]
+            if currSum > maxSum:
+                maxSum = currSum
+            if currSum <= 0:
+                if r < n-1:
+                    l = r + 1
+                    currSum = 0
+            r = r + 1
+        return maxSum
