b7a9dd3915e4ada29cbbc826f160fed23c4bcbd1
Anshul <51377801+agrawal-anshul@users.noreply.github.com>
2025-09-18 20:27:22 +0530

    Time: 682 ms (9.29%), Space: 19.6 MB (47.1%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,17 +1,34 @@
 import heapq
 class Solution(object):
+    # # mysolution:
+    # def findKthLargest(self, nums, k):
+    #     """
+    #     :type nums: List[int]
+    #     :type k: int
+    #     :rtype: int
+    #     """
+    #     minHeap = []
+    #     for x in nums:
+    #         if len(minHeap) < k:
+    #             heapq.heappush(minHeap, x)
+    #         else:
+    #             if x > minHeap[0]:
+    #                 heapq.heappop(minHeap)
+    #                 heapq.heappush(minHeap, x)
+    #     return minHeap[0]
+
+    # better solution:
     def findKthLargest(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: int
         """
-        minHeap = []
-        for x in nums:
-            if len(minHeap) < k:
-                heapq.heappush(minHeap, x)
-            else:
-                if x > minHeap[0]:
-                    heapq.heappop(minHeap)
-                    heapq.heappush(minHeap, x)
-        return minHeap[0]
+        min_heap = []
+        
+        for num in nums:
+            heapq.heappush(min_heap, num)
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        
+        return min_heap[0]
